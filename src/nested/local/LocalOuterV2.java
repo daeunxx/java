bc5f097dcdcf444afd20c261db3892ca5efe139d
daeun <sss3598@gmail.com>
2024-06-23 17:26:38 +0900

    중첩 클래스 | 지역 클래스

---
+++ src/nested/local/LocalOuterV2.java
@@ -0,0 +1,31 @@
+package nested.local;
+
+public class LocalOuterV2 {
+
+  private int outInstanceValue = 3;
+
+  public void process(int paramValue) {
+    int localValue = 1;
+
+    class LocalPrinter implements Printer {
+
+      int value = 0;
+
+      @Override
+      public void print() {
+        System.out.println("value = " + value);
+        System.out.println("localValue = " + localValue);
+        System.out.println("paramValue = " + paramValue);
+        System.out.println("outInstanceValue = " + outInstanceValue);
+      }
+    }
+
+    LocalPrinter printer = new LocalPrinter();
+    printer.print();
+  }
+
+  public static void main(String[] args) {
+    LocalOuterV2 localOuter = new LocalOuterV2();
+    localOuter.process(2);
+  }
+}
