296f3b910c65814f3becd26c1ad95dbbdc71165e
daeun <sss3598@gmail.com>
2024-07-02 00:09:06 +0900

    Generic | 비제한 와일드카드

---
+++ src/generic/ex5/WildCarMain1.java
@@ -0,0 +1,19 @@
+package generic.ex5;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildCarMain1 {
+
+  public static void main(String[] args) {
+    Box<Object> objectBox = new Box<>();
+    Box<Dog> dogBox = new Box<>();
+    Box<Cat> catBox = new Box<>();
+
+    dogBox.setValue(new Dog("멍멍이", 100));
+    WildCardEx.printGenericV1(dogBox);
+    WildCardEx.printGenericV2(dogBox);
+    Dog dog = WildCardEx.printAndReturnGeneric(dogBox);
+    System.out.println("dog = " + dog);
+  }
+}
