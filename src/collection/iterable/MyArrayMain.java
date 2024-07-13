bdd24fab82e9fdc5297de86c6ac11215926f00fb
daeun <sss3598@gmail.com>
2024-07-13 09:56:53 +0900

    순회 | Iterable, Iterator: for-each 가능

--- src/collection/iterable/MyArrayMain.java
+++ src/collection/iterable/MyArrayMain.java
@@ -8,8 +8,14 @@ public class MyArrayMain {
     MyArray myArray = new MyArray(new int[]{1, 2, 3, 4});
     Iterator<Integer> iterator = myArray.iterator();
     System.out.println("iterator 사용");
+
     while (iterator.hasNext()) {
       Integer value = iterator.next();
+      System.out.println("value = " + value);
+    }
+
+    System.out.println("for-each 사용");
+    for (Integer value : myArray) {
       System.out.println("value = " + value);
     }
   }
