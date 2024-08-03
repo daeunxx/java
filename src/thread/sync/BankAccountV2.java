3b146ebbbdde4ad5c32e220f3bc65edd3edc9281
daeun <sss3598@gmail.com>
2024-08-03 10:31:58 +0900

    동기화 | 출금 synchronized 코드 블럭

--- src/thread/sync/BankAccountV2.java
+++ src/thread/sync/BankAccountV2.java
@@ -15,6 +15,7 @@ public class BankAccountV2 implements BankAccount{
   public synchronized boolean withdraw(int amount) {
     log("거래 시작: " + getClass().getSimpleName());
 
+    //==임계 영역 시작==//
     log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
     if (balance < amount) {
       log("[검증 실패]");
@@ -25,6 +26,7 @@ public class BankAccountV2 implements BankAccount{
     sleep(1000); //출금에 걸리는 시간으로 가정
     balance -= amount;
     log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+    //==임계 영역 종료==//
 
     log("거래 종료");
     return true;
