c2c4b2fa7c7f22bc88ee166425123c0103f0c075
daeun <sss3598@gmail.com>
2024-11-13 15:44:27 +0900

    네트워크 | 예제6: SessionManager 사용

---
+++ src/network/tcp/v6/ClientV6.java
@@ -0,0 +1,45 @@
+package network.tcp.v6;
+
+import static util.MyLogger.log;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientV6 {
+
+  private static final int PORT = 12345;
+
+  public static void main(String[] args) {
+    log("클라이언트 시작");
+
+    try (Socket socket = new Socket("localhost", PORT);
+        DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
+
+      log("소켓 연결: " + socket);
+
+      Scanner scanner = new Scanner(System.in);
+      while (true) {
+        System.out.print("전송 문자: ");
+        String toSend = scanner.nextLine();
+
+        // 서버에게 문자 전송
+        outputStream.writeUTF(toSend);
+        log("client -> server: " + toSend);
+
+        if (toSend.equals("exit")) {
+          break;
+        }
+
+        // 서버로부터 문자 받기
+        String received = inputStream.readUTF();
+        log("server -> client: " + received);
+      }
+    } catch (IOException e) {
+      log(e);
+    }
+  }
+}
