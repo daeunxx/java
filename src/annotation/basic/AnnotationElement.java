d34cef2f8b0d74e400ec985b9dcf4e261cdd1681
daeun <sss3598@gmail.com>
2024-11-20 08:59:05 +0900

    애노테이션 | 애노테이션 정의

---
+++ src/annotation/basic/AnnotationElement.java
@@ -0,0 +1,17 @@
+package annotation.basic;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import util.MyLogger;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnnotationElement {
+
+  String value();
+  int count() default 0;
+  String[] tags() default {};
+
+  // MyLogger myLogger 선언 불가
+  Class<? extends MyLogger> logger() default MyLogger.class; // 클래스 정보만 가능
+
+}
---
+++ src/annotation/basic/ElementData3.java
@@ -0,0 +1,6 @@
+package annotation.basic;
+
+@AnnotationElement("data3")
+public class ElementData3 {
+
+}
