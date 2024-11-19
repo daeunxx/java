e2e1613a975c6eb17a3e762a4205959ad7a0705a
daeun <sss3598@gmail.com>
2024-11-19 22:45:01 +0900

    HTTP 서버 | V6: 리플렉션 서블릿

--- src/was/httpserver/ServletManager.java
+++ src/was/httpserver/ServletManager.java
@@ -10,11 +10,16 @@ import was.httpserver.servlet.NotFoundServlet;
 public class ServletManager {
 
   private final Map<String, HttpServlet> servlets = new HashMap<>();
+  private HttpServlet defaultServlet = new DefaultServlet();
   private HttpServlet notFoundServlet = new NotFoundServlet();
   private HttpServlet internalErrorServlet = new InternalErrorServlet();
 
   public void add(String path, HttpServlet servlet) {
     servlets.put(path, servlet);
+  }
+
+  public void setDefaultServlet(HttpServlet defaultServlet) {
+    this.defaultServlet = defaultServlet;
   }
 
   public void setNotFoundServlet(HttpServlet notFoundServlet) {
@@ -27,7 +32,7 @@ public class ServletManager {
 
   public void execute(HttpRequest request, HttpResponse response) throws IOException {
     try {
-      HttpServlet servlet = servlets.getOrDefault(request.getPath(), new DefaultServlet());
+      HttpServlet servlet = servlets.getOrDefault(request.getPath(), defaultServlet);
       servlet.service(request, response);
     } catch (PageNotFoundException e) {
       e.printStackTrace();
