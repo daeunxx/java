1c7f6f851c5113a177f85433b9b9deee8db6a479
daeun <sss3598@gmail.com>
2024-11-20 21:53:49 +0900

    HTTP 서버 | V9: 성능 최적화

--- src/was/v8/ServerMainV8.java
+++ src/was/v8/ServerMainV8.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.List;
 import was.httpserver.HttpServer;
 import was.httpserver.ServletManager;
-import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 
 public class ServerMainV8 {
 
@@ -12,7 +12,8 @@ public class ServerMainV8 {
 
   public static void main(String[] args) throws IOException {
     ServletManager servletManager = new ServletManager();
-    servletManager.setDefaultServlet(new AnnotationServletV2(List.of(new SiteControllerV8())));
+    //servletManager.setDefaultServlet(new AnnotationServletV2(List.of(new SiteControllerV8())));
+    servletManager.setDefaultServlet(new AnnotationServletV3(List.of(new SiteControllerV8())));
 
     HttpServer httpServer = new HttpServer(PORT, servletManager);
     httpServer.start();
